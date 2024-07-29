39bd6c014ae53d9eff59246a88fc464f43d3036b
Theodryden <theodryden@yahoo.com>
2024-07-29 17:38:39 +0100

    Updated Blog and footer sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Footer from './components/footer/Footer'
+import BlogSection from './sections/blogsection/BlogSection'
 import Header from './sections/headersection/Header'
-import InfoSection from './sections/infosection/InfoSection'
-import AboutEvents from './sections/abouteventssection/AboutEvents'
 import QuickLinks from './sections/quicklinkssection/QuickLinks'
 import TreatmentSection from './sections/treatmentsection/TreatmentSection'
 // import NavBar from './components/nav-bar/NavBar'
@@ -12,12 +11,10 @@ import TreatmentSection from './sections/treatmentsection/TreatmentSection'
 const page = () => {
   return (
     <div>
-
       <Header />
       <QuickLinks />
       <TreatmentSection />
-      <AboutEvents />
-      <InfoSection />
+      <BlogSection />
       <Footer />
     </div>
   )
